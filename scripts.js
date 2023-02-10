e6e98598d9dfeeeb4dfb2eb5eb66c28b023e2957
Alec Garza <alecmgarza@gmail.com>
2023-02-09 20:32:33 -0600

    Render gameboard array content onto gameboard.

--- scripts.js
+++ scripts.js
@@ -1,18 +1,31 @@
-const body = document.getElementsByName('body');
-
-const gameboard = (() => {
+const Gameboard = (() => {
     let gameboard = [ "x", "o", "x", "o", "x", "o", "x", "o", "x"];
 
-    const board = document.createElement('div');
-    board.setAttribute('id', 'gameboard');
-    document.body.appendChild(board);
+    const gameboardContent = () => {
+        const gameboard = document.createElement('div');
+        gameboard.setAttribute('id', 'gameboard');
+        document.body.appendChild(gameboard);
 
-    for (i = 1; i <= 9; i++) {
-        let boardSquare = document.createElement('div');
-        boardSquare.setAttribute('class', 'board-square');
-        boardSquare.setAttribute('id', i);
-        board.appendChild(boardSquare);
-    }
+        for (let i = 0; i <= 8; i++) {
+            let boardSquare = document.createElement('div');
+            boardSquare.setAttribute('class', 'board-square');
+            boardSquare.setAttribute('id', i);
+            gameboard.appendChild(boardSquare);
+            boardSquare.textContent = Gameboard.gameboard[i];
+        };
+    };
 
-    return {gameboard};
+    return {gameboard, gameboardContent};
+})();
+
+const game = (() => {
+    const markBoard = () => {
+        for (let i = 0; i < Gameboard.gameboard.length; i++) {
+            return Gameboard.gameboard[i];
+        };
+    };
+
+    Gameboard.gameboardContent(markBoard());
+
+    return {markBoard};
 })();
