1e095a4c9d860d03b3d205a46132df7836ccf03d
Alec Garza <alecmgarza@gmail.com>
2023-02-20 19:34:29 -0600

    Start button opens a modal to enter player names.

--- scripts.js
+++ scripts.js
@@ -5,6 +5,7 @@ const Gameboard = (() => {
         const gameboard = document.createElement('div');
         gameboard.setAttribute('id', 'gameboard');
         document.body.appendChild(gameboard);
+        document.body.insertBefore(gameboard, document.body.firstChild);
 
         for (let i = 0; i <= 8; i++) {
             let boardSquare = document.createElement('div');
@@ -17,6 +18,9 @@ const Gameboard = (() => {
 
     return {gameboard, gameboardContent};
 })();
+
+const boardSquare = document.getElementById('gameboard');
+
 
 const game = (() => {
     const markBoard = () => {
@@ -31,10 +35,12 @@ const game = (() => {
 
     const startButton = document.getElementById('start');
     const restartButton = document.getElementById('restart');
+    const boardSquare = document.getElementsByClassName('board-square');
 
     startButton.onclick = () => {
         startButton.style.display = 'none';
         restartButton.style.display = 'block';
+        player1Turn();    
     }
 
     restartButton.onclick = () => {
@@ -42,19 +48,51 @@ const game = (() => {
         restartButton.style.display = 'none';
     }
 
-    const playerTurn = () => {
-
+    const player1Turn = () => {
+        player1.turn = true;
+        player2.turn = false;
+        boardSquare.onclick = () => {
+            console.log('It is player 2 turn');
+            player2Turn();
+        }
+        
     };
+
+    const player2Turn = () => {
+        player2.turn = true;
+        player1.turn = false;
+        boardSquare.onclick = () => {
+            player1Turn();
+        }
+    }
 
     Gameboard.gameboardContent(markBoard());
 
     return {markBoard};
 })();
 
-const Player = (name, mark) => {
+const Player = (name, mark, turn) => {
 
-    return {name, mark};
+    const startButton = document.getElementById('start');
+    const modal = document.getElementById('modal');
+
+    startButton.onclick = () => {
+        modal.style.display = 'block';
+    }
+
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+
+    return {name, mark, turn};
 }
 
-const player1 = Player('Player1', 'x');
-const player2 = Player('Player2', 'o');
+const player1 = Player('Player1', 'x', true);
+const player2 = Player('Player2', 'o', false);
+
+/* 
+Things to do:
+Figure out why onclick does not trigger player turn function (boardSquare not being recognized)
+*/
