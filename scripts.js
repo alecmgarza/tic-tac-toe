f9032f367a860ac06ffbe910f00c8a8c6f52658d
Alec Garza <alecmgarza@gmail.com>
2023-03-04 17:03:53 -0600

    Message displays when a player wins the game.

--- scripts.js
+++ scripts.js
@@ -27,6 +27,7 @@ const game = (() => {
     const gameboard = document.getElementById('gameboard');
     const boardSquares = document.getElementsByClassName('board-squares');
     const submit = document.getElementById('submit');
+    const winMessage = document.querySelector('h2');
 
     startButton.onclick = () => {
         startButton.style.display = 'none';
@@ -39,8 +40,8 @@ const game = (() => {
         restartButton.style.display = 'none';
         Gameboard.gameboard = '';
         Gameboard.renderMarks();
+        winMessage.textContent = '';
     }
-
 
     submit.onclick = () => {
         modal.style.display = 'none';
@@ -52,25 +53,25 @@ const game = (() => {
         }
     }
 
-    
-    const namePlayer1 = () => {    
+    const namePlayer1 = () => {   
+        const p1Hub = document.getElementById('p1-hub-name'); 
         player1.name = document.getElementById('p1-name').value;
+        p1Hub.textContent = player1.name;
     };
 
     const namePlayer2 = () => {
+        const p2Hub = document.getElementById('p2-hub-name');
         player2.name = document.getElementById('p2-name').value;
+        p2Hub.textContent = player2.name;
     }
 
     const createPlayers = () => {
-        const p1Hub = document.getElementById('p1-hub-name');
-        const p2Hub = document.getElementById('p2-hub-name');
         submit.onclick = () => {
             modal.style.display = 'none';
+
             namePlayer1();
             namePlayer2();
             markBoard();
-            p1Hub.textContent = player1.name;
-            p2Hub.textContent = player2.name;
         }
     }
 
@@ -79,8 +80,6 @@ const game = (() => {
         console.log(`It's ${player1.name}'s turn!`)
         player2.turn = false;
     };
-
-    
 
     const player2Turn = () => {
         player1.turn = false;
@@ -108,9 +107,37 @@ const game = (() => {
                     Gameboard.gameboard[i] = player2.mark;
                 }
                 Gameboard.renderMarks();
+                endGame();
             }
         }
     };
+
+    const endGame = () => {
+        
+        if (Gameboard.gameboard[0,1,2] || 
+            Gameboard.gameboard[3,4,5] || 
+            Gameboard.gameboard[6,7,8] || 
+            Gameboard.gameboard[0,3,6] ||
+            Gameboard.gameboard[1,4,7] ||
+            Gameboard.gameboard[2,5,8] ||
+            Gameboard.gameboard[0,4,8] ||
+            Gameboard.gameboard[2,4,6] == 'x') {
+
+            winMessage.textContent = `${player1.name} wins!`;
+
+        } else if (Gameboard.gameboard[0,1,2] || 
+            Gameboard.gameboard[3,4,5] || 
+            Gameboard.gameboard[6,7,8] || 
+            Gameboard.gameboard[0,3,6] ||
+            Gameboard.gameboard[1,4,7] ||
+            Gameboard.gameboard[2,5,8] ||
+            Gameboard.gameboard[0,4,8] ||
+            Gameboard.gameboard[2,4,6] == 'o') {
+            
+            winMessage.textContent = `${player2.name} wins!`;
+
+        }
+    }
 
     return {takeTurns, createPlayers};
 })();
@@ -119,6 +146,10 @@ game.createPlayers();
 game.takeTurns();
 
 /* 
-Things to do:
-Create player and make the variable public.
+Bugs:
+Player can change the mark of a spot on the board to their own.
+
+Still need:
+- Display wins.
+- Style page.
 */
